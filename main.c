d41ecec4d31119094a9d546284b706c8b840e706
Heng Li <lh3@me.com>
2021-03-27 16:29:02 -0400

    expose -f

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ static void print_usage(FILE *fp, const pt_pdopt_t *po, const pt_svopt_t *so)
 	fprintf(stderr, "  -n INT     inspect top INT edges [%d]\n", so->topn);
 	fprintf(stderr, "  -s INT     RNG seed [%ld]\n", (long)so->seed);
 	fprintf(stderr, "  -p INT     rounds of perturbations [%d]\n", so->n_perturb);
+	fprintf(stderr, "  -f FLOAT   fraction to flip for perturbation [%.3g]\n", so->f_perturb);
 }
 
 int main(int argc, char *argv[])
@@ -28,13 +29,14 @@ int main(int argc, char *argv[])
 	pt_realtime();
 	pt_pdopt_init(&po);
 	pt_svopt_init(&so);
-	while ((c = ketopt(&o, argc, argv, 1, "k:w:c:n:s:p:", 0)) >= 0) {
+	while ((c = ketopt(&o, argc, argv, 1, "k:w:c:n:s:p:f:", 0)) >= 0) {
 		if (c == 'k') po.k = atoi(o.arg);
 		else if (c == 'w') po.w = atoi(o.arg);
 		else if (c == 'c') po.max_occ = atoi(o.arg);
 		else if (c == 'n') so.topn = atoi(o.arg);
 		else if (c == 's') so.seed = atol(o.arg);
 		else if (c == 'p') so.n_perturb = atoi(o.arg);
+		else if (c == 'f') so.f_perturb = atof(o.arg);
 	}
 	if (o.ind == argc) {
 		print_usage(stderr, &po, &so);
